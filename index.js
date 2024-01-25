0ed5cad804f60a263f5697e7cb12d7f86d8646dd
RoufRain <roufrain91@gmail.com>
2024-01-25 22:25:38 +0600

    donation requests added

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //to database
+    const donationRequestCollection = client
+      .db("bloodDonationDB")
+      .collection("donationRequest");
+
+    //for donation req collection
+    app.get("/donationRequest", async (req, res) => {
+      const cursor = donationRequestCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
